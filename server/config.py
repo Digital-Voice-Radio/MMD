e5030a55f3780dcd7730633359fd957c5f311c0d
Jared Quinn <jared@jaredquinn.info>
2025-08-04 23:28:54 +0000

    Configuration File

---
+++ server/config.py
@@ -0,0 +1,12 @@
+
+CONFIG = {
+        'DASHBOARD': {
+            'Title': 'NZSIP Extension Status',
+            'Links': [
+                ('DVNZ', 'https://dvnz.nz'),
+                ('Discord', 'https://discord.gg/87hz8GYa'),
+            ],
+            'Copyright': '(C) Copyright 2025, NZSIP/Digital Voice New Zealand',
+        },
+        'Port': 8891,
+}
