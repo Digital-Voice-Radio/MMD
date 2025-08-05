6a494522bcc3b6b0ae5d6170b50f45f0ed09f021
Jared Quinn <jared@jaredquinn.info>
2025-08-05 01:18:48 +0000

    Updated sample config

--- server/config_SAMPLE.py
+++ server/config_SAMPLE.py
@@ -2,6 +2,7 @@
 CONFIG = {
         'DASHBOARD': {
             'Title': 'NZSIP Extension Status',
+            'Logo': 'https://nzsip.nz/static/img/dvnz-96x96.png',
             'Links': [
                 ('DVNZ', 'https://dvnz.nz'),
                 ('Discord', 'https://discord.gg/87hz8GYa'),
